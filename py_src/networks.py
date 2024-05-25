8a3bb02a14300fe5f3aefae32d717f4c42145031
PatelRaj8694 <rajprashantpatel2696@gmail.com>
2024-05-26 11:56:03 +1200

    chnaging dnns

--- py_src/networks.py
+++ py_src/networks.py
@@ -109,24 +109,33 @@ class dnn(nn.Module):
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.xavier_uniform_(self.fc2.weight)
         nn.init.xavier_uniform_(self.fc3.weight)
+        nn.init.xavier_uniform_(self.fc4.weight)
+        nn.init.xavier_uniform_(self.fc5.weight)
+        nn.init.xavier_uniform_(self.fc6.weight)
         nn.init.xavier_uniform_(self.out.weight)
 
-    def __init__(self, G_in, G_out, w1, w2, w3):
+    def __init__(self, G_in, G_out, w1, w2, w3, w4, w5, w6):
         super(dnn, self).__init__()
         
         self.fc1= nn.Linear(G_in, w1)
         self.fc2= nn.Linear(w1, w2)
         self.fc3= nn.Linear(w2, w3)
-        self.out= nn.Linear(w3, G_out)
+        self.fc4= nn.Linear(w3, w4)
+        self.fc5= nn.Linear(w4, w5)
+        self.fc6= nn.Linear(w5, w6)
+        self.out= nn.Linear(w6, G_out)
 
-        # self.weight_init()
+        self.weight_init()
         
     def forward(self, x):
         
         # x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
+        x = F.leaky_relu(self.fc4(x))
+        x = F.leaky_relu(self.fc5(x))
+        x = F.leaky_relu(self.fc6(x))
         x = F.sigmoid(self.out(x))
         # x = x.view(1, 1, 1000, 25)
         return x
