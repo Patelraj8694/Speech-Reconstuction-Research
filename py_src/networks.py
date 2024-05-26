d0c6e5b27d64e6ec3f9763bdf3b5125a471925ab
PatelRaj8694 <rajprashantpatel2696@gmail.com>
2024-05-26 12:29:38 +1200

    added regularisation in dnns

--- py_src/networks.py
+++ py_src/networks.py
@@ -114,30 +114,39 @@ class dnn(nn.Module):
         nn.init.xavier_uniform_(self.fc6.weight)
         nn.init.xavier_uniform_(self.out.weight)
 
-    def __init__(self, G_in, G_out, w1, w2, w3, w4, w5, w6):
+    def __init__(self, G_in, G_out, w1, w2, w3, w4, w5, w6, dropout_rate=0.3):
         super(dnn, self).__init__()
         
-        self.fc1= nn.Linear(G_in, w1)
-        self.fc2= nn.Linear(w1, w2)
-        self.fc3= nn.Linear(w2, w3)
-        self.fc4= nn.Linear(w3, w4)
-        self.fc5= nn.Linear(w4, w5)
-        self.fc6= nn.Linear(w5, w6)
-        self.out= nn.Linear(w6, G_out)
+        self.fc1 = nn.Linear(G_in, w1)
+        self.bn1 = nn.BatchNorm1d(w1)
+        self.fc2 = nn.Linear(w1, w2)
+        self.bn2 = nn.BatchNorm1d(w2)
+        self.fc3 = nn.Linear(w2, w3)
+        self.bn3 = nn.BatchNorm1d(w3)
+        self.fc4 = nn.Linear(w3, w4)
+        self.bn4 = nn.BatchNorm1d(w4)
+        self.fc5 = nn.Linear(w4, w5)
+        self.bn5 = nn.BatchNorm1d(w5)
+        self.fc6 = nn.Linear(w5, w6)
+        self.bn6 = nn.BatchNorm1d(w6)
+        self.out = nn.Linear(w6, G_out)
+        self.dropout = nn.Dropout(dropout_rate)
 
         self.weight_init()
         
     def forward(self, x):
-        
-        # x = x.view(x.size(0), -1)
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
-        x = F.leaky_relu(self.fc4(x))
-        x = F.leaky_relu(self.fc5(x))
-        x = F.leaky_relu(self.fc6(x))
+        x = F.leaky_relu(self.bn1(self.fc1(x)))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.bn2(self.fc2(x)))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.bn3(self.fc3(x)))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.bn4(self.fc4(x)))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.bn5(self.fc5(x)))
+        x = self.dropout(x)
+        x = F.leaky_relu(self.bn6(self.fc6(x)))
         x = F.sigmoid(self.out(x))
-        # x = x.view(1, 1, 1000, 25)
         return x
 
 
